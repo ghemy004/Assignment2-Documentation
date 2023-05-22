f11a8a0cb364e76f606ad873ea8a8848b6c80b1f
Matewos Ghebreab <ghemy004@mymail.unisa.edu.au>
2023-05-22 18:02:47 +0930

    created a header

---
+++ OOP-Part2.py
@@ -0,0 +1,9 @@
+'''
+File: OOP-Part2.py
+Description: A brief description of this Python module.
+Author: Matewos Ghebreab
+StudentID: 110350204
+EmailID: ghemy004@mymail.unisa.edu.au
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
+
