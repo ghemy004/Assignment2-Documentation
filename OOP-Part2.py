c563f83ad602330aec7b3621cd95aca11f9b0883
Matewos Ghebreab <ghemy004@mymail.unisa.edu.au>
2023-05-22 19:23:32 +0930

    Created Workshop Class, implemented methods, etc..

--- OOP-Part2.py
+++ OOP-Part2.py
@@ -7,3 +7,41 @@ EmailID: ghemy004@mymail.unisa.edu.au
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
 
+class Workshop: # The main class
+    def __init__(self, enchanter, forge): # 
+        self.forge = forge
+        self.enchanter = enchanter
+        self.materials = []
+        self.weapons = []
+        self.enchantment = [] 
+    
+    def addMaterial(self, material):
+        self.materials.append(material)
+    
+    def removeMaterial(Self, material):
+        self.materials.remove(material)
+    
+    def addWeapon(self, weapon):
+        self.weapons.append(weapon)
+    
+    def removeWeapon(Self, weapon):
+        self.weapons.remove(weapon)
+    
+    def addEchantment(self,echantment):
+        self.enchantment.append(echantment)
+
+    def removeEchantment(self, enchantment):
+        self.enchantment.remove(enchantment)
+    
+    def displayWeapons(self):
+
+
+
+
+    
+    
+
+
+
+
+  
