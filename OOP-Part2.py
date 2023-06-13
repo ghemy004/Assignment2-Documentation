12b5f109aa0b890f682b0cc0c14ab8cbe96c4f93
Matewos Ghebreab <ghemy004@mymail.unisa.edu.au>
2023-06-13 17:28:47 +0930

    import material & abc,created crafter class

--- OOP-Part2.py
+++ OOP-Part2.py
@@ -1,3 +1,7 @@
+from abc import ABC, abstractmethod
+
+import Material
+
 '''
 File: OOP-Part2.py
 Description: A brief description of this Python module.
@@ -9,39 +13,78 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 class Workshop: # The main class
     def __init__(self, enchanter, forge): # 
-        self.forge = forge
-        self.enchanter = enchanter
-        self.materials = []
-        self.weapons = []
-        self.enchantment = [] 
+        self.__forge = forge
+        self.__enchanter = enchanter
+        self.__materials = []
+        self.__weapons = []
+        self.__enchantment = [] 
     
     # this method takes material as a parameter and adds it to the material 
     def addMaterial(self, material):
-        self.materials.append(material)
+        self.__materials.append(material)
     
     def removeMaterial(self, material):
-        self.materials.remove(material)
+        self.__materials.remove(material)
     
     # this method takes weapon as a parameter and adds it to the weapons list
     def addWeapon(self, weapon):
-        self.weapons.append(weapon)
+        self.__weapons.append(weapon)
         
     def removeWeapon(self, weapon):
-        self.weapons.remove(weapon)
+        self.__weapons.remove(weapon)
     
     # this method takes echantment as a parameter and adds it to the echantment list
     def addEchantment(self,echantment):
-        self.enchantment.append(echantment)
+        self.__enchantment.append(echantment)
 
     def removeEchantment(self, enchantment):
-        self.enchantment.remove(enchantment)
-  
-    def displayWeapons(self): 
-        value = " "
-        for weapon in self.weapons:
-            if weapon.enchantment:
-                value += "The {} is imbued with a {}. {}\n".format(weapon.name, weapon.echantment.useEffect(), weapon.attack())
-            else:
-                value += "The {} is not enchanted. {}\n".format(weapon.name, weapon.attack()) 
-        return value
+        self.__enchantment.remove(enchantment)
 
+    def displayWeapons(self):
+        result = '' 
+        
+        for  weapon in self.__weapons: # iterates over each weapon in sel.__weapons list
+            if weapon.enchantment: # checks if weapon has an enchantment by using this in the conditional statement 
+                enchantmentDetails = f"The {weapon.enchantment.useEffect()}"
+            else:
+                enchantmentDetails = f"{self.__weapons} is not enchanted"
+                        
+            weaponDetails = f'The {weapon.name} is {enchantmentDetails}. It deals {weapon.attack()} damage.\n'
+            
+            result += weaponDetails
+        return result
+
+    def displayEnchantments(self):
+        enchantmentNames = []
+        
+        for enchantment in self.__enchantment:
+            enchantmentNames.append('A {} enchantment is stored in workshop.'.format(enchantment.getName()))
+
+        return '\n'.join(enchantmentNames)
+
+class Crafter(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def craft(self): # implementation of the craft method
+        pass
+
+    @abstractmethod    
+    def disassemble(self): # implemenetation of the disassemble method
+        pass
+
+
+class Forge(Crafter):
+    def __init__(self):
+        
+        
+
+
+
+
+
+materials[primaryMaterial.__class__.__name__] -= 1
+materials[catalystMaterial.__class__.__name__] -= 1
+materials[primaryMaterial.__class__.__name__] += 1
+materials[catalystMaterial.__class__.__name__] += 1
